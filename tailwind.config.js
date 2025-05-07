c036555fba0b9b12995fc9d4793043cdce939710
Jacob Corum <jacob.w.corum@gmail.com>
2025-05-07 16:23:26 -0400

    fix tailwind so sp and rp colors show correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: ["text-indigo-700", "text-emerald-700"],
   theme: {
     extend: {},
   },
